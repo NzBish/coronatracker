bb6608e27740e456376a7123e43f7513f4084ed4
Bish <chrisbishop@slingshot.co.nz>
2020-05-12 10:33:00 +1200

    Updates

---
+++ precache-manifest.c8f02aa5af197a3d22856a3a7c092233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2d4574a08b3a1016e4f57588e15938",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "d7f113ab43e6b0e8f4c9",
+    "url": "/coronatracker/static/css/main.d7273da6.chunk.css"
+  },
+  {
+    "revision": "2924ccb0e41df36d8d0e",
+    "url": "/coronatracker/static/js/2.1c7c521e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatracker/static/js/2.1c7c521e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f113ab43e6b0e8f4c9",
+    "url": "/coronatracker/static/js/main.8708e008.chunk.js"
+  },
+  {
+    "revision": "c4e9d4ac3550c033ed01",
+    "url": "/coronatracker/static/js/runtime-main.51613ec7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/image.d7265326.png"
+  }
+]);
